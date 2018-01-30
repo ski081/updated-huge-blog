54d04fdd194bea4cc6d94936e6322990619f3df3
Mark Struzinski <ski081@gmail.com>
2018-01-29 21:08:52 -0500

    Adding initial scss compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
+var gulp            = require('gulp');
+var gutil           = require('gulp-util');
+var sass            = require('gulp-sass');
+var autoprefixer    = require('gulp-autoprefixer');
 
-gulp.task('default', function() {
-    return gutil.log('Gulp is running')
+gulp.task('scss', function() {
+    gulp.src('src/scss/**/*.scss')
+        .pipe(sass({
+            outputstyle: 'compressed'
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 20 versions']
+        }))
+        .pipe(gulp.dest('static/css'));
 });
+
+gulp.task('watch', ['scss'], function() {
+    gulp.watch('src/scss/**/*', ['scss']);
+});
+
+gulp.task('default', ['watch']);
