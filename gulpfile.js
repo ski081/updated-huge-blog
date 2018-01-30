5a68eb33a3bd5ce9547921b8fe70a0436adb3966
Mark Struzinski <ski081@gmail.com>
2018-01-29 22:29:58 -0500

    Adding scss compilation

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var del             = require('del');
 
 // Path variables
 var SASS_SRC_PATH       = 'src/scss/**/*.scss';
+var SASS_PATH           = 'static/css/**/*';
 var SASS_DEST_PATH      = 'static/css';
 var IMAGES_SRC_PATH     = 'src/images/**/*';
 var IMAGES_PATH         = 'static/images/**/*';
@@ -16,13 +17,16 @@ var JS_DEST_PATH        = 'static/js';
 
 // Compile and compress sass files. Deploy to static folder
 gulp.task('scss', function() {
+    del([SASS_PATH]);
+
     gulp.src(SASS_SRC_PATH)
         .pipe(sass({
             outputstyle: 'compressed'
         }))
         .pipe(autoprefixer({
             browsers: ['last 20 versions']
-        }));
+        }))
+        .pipe(gulp.dest(SASS_DEST_PATH));
 });
 
 gulp.task('watch', ['scss'], function() {
