0688108e72bac3d82dbaed18f245d3c36ec68227
Mark Struzinski <ski081@gmail.com>
2018-01-29 22:11:13 -0500

    Removing hash tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp            = require('gulp');
 var gutil           = require('gulp-util');
 var sass            = require('gulp-sass');
 var autoprefixer    = require('gulp-autoprefixer');
-var hash            = require('gulp-hash');
 var del             = require('del');
 
 // Path variables
@@ -23,31 +22,11 @@ gulp.task('scss', function() {
         }))
         .pipe(autoprefixer({
             browsers: ['last 20 versions']
-        }))
-        .pipe(hash())
-        .pipe(gulp.dest(SASS_DEST_PATH));
+        }));
 });
 
-// Hash images
-gulp.task('images', function() {
-    del([IMAGES_PATH]);
-    gulp.src(IMAGES_SRC_PATH)
-        .pipe(hash())
-        .pipe(gulp.dest(IMAGES_DEST_PATH));
-});
-
-// Hash Javascript
-gulp.task('js', function() {
-    del([JS_PATH]);
-    gulp.src(JS_SRC_PATH)
-        .pipe(hash())
-        .pipe(gulp.dest(JS_DEST_PATH));
-});
-
-gulp.task('watch', ['scss', 'images', 'js'], function() {
+gulp.task('watch', ['scss'], function() {
     gulp.watch(SASS_SRC_PATH, ['scss']);
-    gulp.watch(IMAGES_SRC_PATH, ['images']);
-    gulp.watch(JS_SRC_PATH, ['js']);
 });
 
 gulp.task('default', ['watch']);
